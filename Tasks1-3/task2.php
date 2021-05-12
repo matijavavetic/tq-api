5713dbfb96ba09d13ef0dfaa61a9b38c0f084cdc
Matija Vavetic <matija.vavetic@gmail.com>
2021-05-12 14:25:36 +0200

    Sources for caching systems comparison

--- Tasks1-3/task2.php
+++
@@ -1,9 +0,0 @@
-<?
-
-function reformat(string $input): string {
-    $vowels = ['a', 'e', 'i', 'o', 'u'];
-
-    $formattedOutput = ucfirst(strtolower(str_ireplace($vowels, '', $input)));
-
-    return print($formattedOutput);
-}
